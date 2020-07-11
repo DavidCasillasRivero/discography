6e79ccbcb37b688b1451dfc50fe18476f63aba4e
David Casillas Rivero <>
2020-07-11 13:40:37 +0200

    Updates

---
+++ precache-manifest.baeb099795212c65e8ec0bfdf35b1a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb9fe58ee34acd41c55cddb2b8ef1e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "863f3e5cd6c84131ef0a",
+    "url": "/static/css/main.3c555234.chunk.css"
+  },
+  {
+    "revision": "82776d0729be1024512f",
+    "url": "/static/js/2.ce9c9f47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce9c9f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863f3e5cd6c84131ef0a",
+    "url": "/static/js/main.6f482f21.chunk.js"
+  },
+  {
+    "revision": "06976168a1b4f04b59fa",
+    "url": "/static/js/runtime-main.13b51ba4.js"
+  }
+]);
